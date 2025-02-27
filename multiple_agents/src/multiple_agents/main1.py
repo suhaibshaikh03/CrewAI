ff23c03fcd204c62e7b8324b699736959808583c
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2025-02-27 22:40:45 +0500

    Add files via upload

---
+++ multiple_agents/src/multiple_agents/main1.py
@@ -0,0 +1,20 @@
+from crewai.flow.flow import start, listen, Flow
+from multiple_agents.crews.dev_crew.dev_crew import DevCrew
+
+class DevFlow(Flow):
+    @start()
+    def run_dev_crew(self):
+        output = (
+            DevCrew()
+            .crew()
+            .kickoff(inputs={"problem":"write python code for addition two numbers"})
+        )
+        return output.raw
+    
+
+
+def kickoff():
+    obj = DevFlow()
+    result = obj.kickoff()
+    print(result)
+
